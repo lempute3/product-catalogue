4e548ecdda9af5046f3a9772340071515bd1b331
lempute3 <21333005+lempute3@users.noreply.github.com>
2023-10-18 15:06:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost'
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
